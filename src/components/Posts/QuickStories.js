a357b9daaf675ebaaaac991c1f55082a7ce3abec
AncherHisogi <anchertunchar@gmail.com>
2022-02-25 16:49:01 +0200

    fixed an issue where quick stories would have the same profile image

--- src/components/Posts/QuickStories.js
+++ src/components/Posts/QuickStories.js
@@ -4,44 +4,32 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
+import GetStories from './GetStories';
 
 
-export default class QuickStories extends React.Component {
-  constructor(props) {
-    super(props);
+export default function QuickStories() {
 
-    this.state = {person: [], person2: []};
-  }
-
-  componentDidMount(){
-    fetch('https://randomuser.me/api/')
-        .then(res => res.json())
-        .then(({ results }) => this.setState({ person: results}))
-  }
-
-  render (){
-    var img
-    var persons = this.state.person.map((item, i) =>(
-      img = item.picture.thumbnail
-    ))
   
     return (
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={0} justifyContent="center">
           <Grid item xs={2}>
-            <Avatar alt="test1" src={img}>+</Avatar>
+            
+            <GetStories />
           </Grid>
           <Grid item xs={2}>
-            <Avatar alt="test2" src={img}> </Avatar>
+            <GetStories />
           </Grid>
           <Grid item xs={2}>
-            <Avatar alt="test3" src={img} >S</Avatar>
+          <GetStories />
+            
           </Grid>
           <Grid item xs={2}>
-            <Avatar alt="test4" src={img}>N</Avatar> 
+          <GetStories />
+            
           </Grid>
         </Grid>
       </Box>
     );
-  }
+  
 }
