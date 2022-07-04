836d822fd9c96b1200f2069c815a98bd216302a5
Ancher Myumyun <anchertunchar@gmail.com>
2022-07-04 19:21:24 +0000

    updated friendlist to be more compact

--- src/components/Friends/FriendCard.js
+++ src/components/Friends/FriendCard.js
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import GetFriends from './GetFriends'
-const imgUrl = ['https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg']
 
 // const useStyles = makeStyles({
 //     avatarPicture:{
